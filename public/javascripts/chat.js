ffcba92a5f7ddd9656ffe365ca8cde29dc4b257c
borlyn <druidchanambam@gmail.com>
2014-07-24 01:13:21 +0530

    landing to chatroom connected

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,36 +1,43 @@
-
-
 //var io = require('socket.io');
-var socket = io.connect('/');
-socket.on('name_set', function(data) {
+var chatSys = io.connect('/chat_Sys'),
+    chatCom = io.connect('/chat_com');
+
+var roomName = decodeURI(
+  (RegExp("room" + '=' + '(.+?) (&|$)').exec(location.search) || [,null])[1]);
+
+if (roomName) {
+  chatSys.on('name_set', function(data) {
+    chatSys.emit('join_room', {'name':roomName});
+  chatSys.on('user_entered', function(user) {
+    $('#messages').append('<div class = "systemMessages">' + user.name + ' has joined the room.' + '</div>');
+  });
+
+  chatSys.on('message', function(message) {
+    var message = JSON.parse(message);
+    $('#messages').append('<div class="' + message.type + '">' + message.message + '</div>');
+  });
+
+  chatCom.on('message', function(message) {
+    var message = JSON.parse(message);
+      $('#messages').append('<div class="' + message.type + '"><span class = "name">' + message.username + ':</span> ' + message.message + '</div>');
+  });
+
   $('#nameform').hide();
-  $('#messages').append('<div class ="systemMessage">' + 'Hello '+data.name+'</div>');
+  $('#messages').append('<div class = "systemMessage">Hello ' + data.name + '</div>');
+ 
   $('#send').click(function() {
     var data = {
-      message: $('#message').val(),
-      type: 'userMessage'
+      message:$('#message').val(),
+      type:'userMessage'
     };
-  socket.send(JSON.stringify(data));
-  $('#message').val('');
-  socket.on('user_entered', function(user) {
-    $('#messages').append('<div class =  "systemMessage">' + user.name + ' has joined the room.' + '</div>');
+    chatCom.send(JSON.stringify(data));
+    $('#message').val('');
   });
-  
 });
+}
 
-
-socket.on('message', function (data) {
-  data = JSON.parse(data);
-  if(data.username){
-    $('#messages').append('<div class = "'+data.type+'"><span class = "name">' +data.username + ":</span> " + data.message + '</div>');
-  } else {
-    $('#message').append('<div class = "'+ data.type + '">' + data.message + '</div>');
-  }
- });
-});
-
-$(document).ready(function() {
+$(function() {
   $('#setname').click(function() {
-    socket.emit("set_name", {name: $('#nickname').val()});//val() is not secure
-});
+    chatSys.emit('set_name', {name: $('#nickname').val()});
+  });
 });
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'rocketchat' });
 });
-
+router.get('/chatroom', function(req, res) {
+  res.render('chatroom', {title: 'rocketchat'});
+});
 module.exports = router;
+
